cba757362bd9f4760c302a61b46e3e7e2f23a57d
Moazam shigri <muhammadmoazzamshigri@gmail.com>
2025-11-30 02:33:52 +0500

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
-
+import "./header.css"
 
 const Header  =() => {
     const [counter, setCounter] = useState(0);
+    const [isOpen, setIsOpen] = useState(false);
     
     
   return (
     <header>
         <div className="logo"><h2>Panto</h2></div>
-        <ul>
+        <ul className='nav-links'>
             <li>
                 <a href="#">Furniture</a>
             </li>
@@ -22,14 +23,31 @@ const Header  =() => {
                 <a href="#">Contact</a>
             </li>
         </ul>
-        <div className="cart">
-            <i class="fa fa-shopping-bag" aria-hidden="true"></i>
+       <div className='fle'>
+         <div className="cart">
+            <i className="fa fa-shopping-bag" aria-hidden="true"></i>
             <div className="counter">
                 {counter}
             </div>
 
 
         </div>
+         <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+    {isOpen 
+      ? <i className='fa fa-close'></i> 
+      : <i className='fa fa-bars'></i>
+    }
+  </div>
+  </div>
+      <div className={`mobile-menu ${isOpen ? "show" : ""}`}>
+        <ul className='ul1'>
+        <li><a href="#">Furniture</a></li>
+        <li><a href="#">Shop</a></li>
+        <li><a href="#">About us</a></li>
+        <li><a href="#">Contact</a></li>
+
+      </ul>
+      </div>
       
     </header>
   )
